37cebb6220164d4e52d2faec770b438bdb2bed74
Joclelson Rodrigues <jocleelson.159@hotmail.com>
2023-10-19 11:31:13 -0300

    feat: :sparkles: configuration babel resolver and paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo']
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ["babel-preset-expo"],
+        plugins: [
+            [
+                "module-resolver",
+                {
+                    root: ["./src"],
+                    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+                    alias: {
+                        "@": "./src",
+                        "@assets": "./src/assets",
+                        "@components": "./src/components",
+                        "@config": "./src/config",
+                        "@constants": "./src/constants",
+                        "@hooks": "./src/hooks",
+                        "@routes": "./src/routes",
+                        "@screens": "./src/screens",
+                        "@services": "./src/services",
+                        "@styles": "./src/styles",
+                        "@utils": "./src/utils",
+                    },
+                },
+            ],
+        ],
+    };
 };
